a3d042f53a4f19807a28fb6d6dc124498560d9fc
khazeenaAli11 <khazeenaali11@gmail.com>
2025-01-04 20:29:08 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let string = "";
+let buttons = document.querySelectorAll('.btn');
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        } else if(e.target.innerHTML == 'AC'){
+            string = "";
+            document.querySelector('input').value = string;
+        } else if(e.target.innerHTML == '←'){
+            string = string.slice(0, -1);
+            document.querySelector('input').value = string;
+        } else if (e.target.innerHTML == '+/-') { // for +/- values and use slice method 
+            if (string) {
+                if (string.startsWith('-')) {
+                    string = string.slice(1); 
+                } else {
+                    string = '-' + string; 
+                }
+                document.querySelector('input').value = string;
+            }
+        } else {
+        console.log(e.target);
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+        }
+    });
+});
